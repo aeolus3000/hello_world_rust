9c33ff7fc0787b57efea7fd2379acbdd695d1072
kopps <simon.kopp@thi.de>
2021-03-30 20:14:45 +0200

    Add more tests and features to stack

--- src/utils/stack.rs
+++ src/utils/stack.rs
@@ -16,26 +16,33 @@ impl Stack {
         stack
     }
 
-    pub fn pop(&mut self) -> i32 {
+    pub fn pop(&mut self) -> Result<i32, &str> {
+        if self.is_empty() {
+            return Err("Can't pop from stack because it is empty")
+        }
         self.pointer -= 1;
-        self.buffer[self.pointer]
+        Ok(self.buffer[self.pointer])
     }
 
-    pub fn push(&mut self, value: i32) {
+    pub fn push(&mut self, value: i32) -> Result<&mut Stack, &str> {
+        if self.is_full() {
+            return Err("Can't push on stack because it is full")
+        }
         self.buffer[self.pointer] = value;
         self.pointer += 1;
+        Ok(self)
     }
 
-    fn is_full(&self,) {
-
+    fn is_full(&self,) -> bool {
+        self.pointer == SIZE
     }
 
-    fn is_empty(&self) {
-
+    fn is_empty(&self) -> bool {
+        self.pointer == 0
     }
 
-    pub fn size(&self) -> i32 {
-        0
+    pub fn size(&self) -> usize {
+        self.pointer
     }
 }
 
@@ -56,7 +63,7 @@ mod stack_test {
         //given: a new stack
         let mut stack = Stack::new();
         assert_eq!(stack.buffer[0], 0);
-        stack.push(value);
+        let _ = stack.push(value);
         assert_eq!(stack.buffer[0], value);
     }
 
@@ -64,7 +71,7 @@ mod stack_test {
     fn pushing_on_stack_should_increment_pointer() {
         //given: a new stack
         let mut stack = Stack::new();
-        stack.push(15);
+        let _ = stack.push(15);
         assert_eq!(stack.pointer, 1);
     }
 
@@ -74,9 +81,9 @@ mod stack_test {
         let mut stack = Stack::new();
         stack.pointer = 3;
         stack.buffer = [11,12,13,0];
-        assert_eq!(stack.pop(), 13);
-        assert_eq!(stack.pop(), 12);
-        assert_eq!(stack.pop(), 11);
+        assert_eq!(stack.pop(), Ok(13));
+        assert_eq!(stack.pop(), Ok(12));
+        assert_eq!(stack.pop(), Ok(11));
     }
 
     #[test]
@@ -84,11 +91,45 @@ mod stack_test {
         //given: a new stack
         let mut stack = Stack::new();
         stack.pointer = 1;
-        stack.pop();
+        let _ = stack.pop();
         assert_eq!(stack.pointer, 0);
         stack.pointer = 2;
-        stack.pop();
+        let _ = stack.pop();
         assert_eq!(stack.pointer, 1);
+    }
+
+    #[test]
+    fn pushing_once_in_the_stack_results_in_expected_size() {
+        //given: a new stack
+        let mut stack = Stack::new();
+        let _ = stack.push(1);
+        let _ = stack.push(2);
+        assert_eq!(stack.size(), 2);
+    }
+
+    #[test]
+    fn stack_is_empty_should_return_empty_state() {
+        //given: a new stack
+        let mut stack = Stack::new();
+        assert_eq!(stack.is_empty(), true);
+        let _ = stack.push(5);
+        assert_eq!(stack.is_empty(), false);
+        let _ = stack.pop();
+        assert_eq!(stack.is_empty(), true);
+    }
+
+    #[test]
+    fn stack_is_full_should_return_full_state() {
+        //given: a new stack
+        let mut stack = Stack::new();
+        assert_eq!(stack.is_full(), false);
+        let _ = stack.push(5);
+        let _ = stack.push(5);
+        let _ = stack.push(5);
+        let _ = stack.push(5);
+        assert_eq!(stack.is_full(), true);
+        let _ = stack.pop();
+        assert_eq!(stack.is_full(), false);
     }
 
 }
